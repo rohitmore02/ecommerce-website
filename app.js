295835944bb18c32592b37ee45127794e96bc874
rohitmore02 <rohitmore386@gmail.com>
2022-12-04 10:17:06 +0530

    product size selection

--- app.js
+++ app.js
@@ -110,5 +110,17 @@ menuItems.forEach((item, index)=>{
 currentProductColors.forEach((color, index) => {
     color.addEventListener("click", () => {
         currentProductImg.src = choosenProduct.colors[index].img;
-    })
-})
+    });
+});
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach(size, () => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        });
+
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
